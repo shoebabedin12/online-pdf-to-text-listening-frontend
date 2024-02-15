cd4e684701d4d42aa2da7e72e6e91235b5b38d66
shoebabedin <shoeb.abedin@intelli.global>
2024-02-15 16:58:25 +0600

    update

--- src/components/TextToSpeech/TextToSpeech.js
+++ src/components/TextToSpeech/TextToSpeech.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 const SpeechToText = () => {
   const { transcript, resetTranscript } = useSpeechRecognition();
+  const [selectedLanguage, setSelectedLanguage] = useState("en-US");
 
   const handleListen = () => {
     if (SpeechRecognition.browserSupportsSpeechRecognition()) {
       SpeechRecognition.startListening({
         continuous: true,
-        language: "bn-BD", // Set the language to Bengali (Bangladesh)
+        language: selectedLanguage,
       });
     } else {
       console.error("Speech recognition not supported in this browser.");
     }
   };
 
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
   return (
     <div>
       <button onClick={handleListen}>Start Listening</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
       <button onClick={resetTranscript}>Reset</button>
+      <select value={selectedLanguage} onChange={handleLanguageChange}>
+        <option value="en-US">English (United States)</option>
+        <option value="bn-BD">Bengali (Bangladesh)</option>
+      </select>
       <p>{transcript}</p>
     </div>
   );
