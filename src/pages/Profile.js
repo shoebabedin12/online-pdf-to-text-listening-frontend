4331ffb7059d957df78924882e9323afa2a2ea4b
shoebabedin <shoeb.abedin@intelli.global>
2024-02-15 15:59:29 +0600

    update

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -20,7 +20,7 @@ import TextToSpeech from "../components/TextToSpeech/TextToSpeech";
 
 const Profile = () => {
   const api = process.env.REACT_APP_API_KEY;
-  const live = process.env.REACT_APP_LIVE;
+  const live = api.split("/")[2];
   const [form] = Form.useForm();
   const { Title } = Typography;
   const [user, setUser] = useState("");
